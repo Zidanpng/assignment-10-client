25bbb5b505847eeae85239a940b68214ed5e324b
Zidanpng <zidanbot3286@gmail.com.com>
2026-02-06 16:14:01 +0800

    details page updated

--- src/pages/Home/Details.jsx
+++ src/pages/Home/Details.jsx
@@ -1,8 +1,11 @@
+import { motion } from "framer-motion";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { IoArrowBack, IoLocationOutline } from "react-icons/io5";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Details = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [item, setItem] = useState(null);
 
   useEffect(() => {
@@ -10,29 +13,90 @@ const Details = () => {
       .then((res) => res.json())
       .then((data) => setItem(data));
   }, [id]);
-  if (!item) return <div className="text-center py-20">Loading...</div>;
+
+  const handleOrder = () => {
+    const orderData = {
+      productId: item._id,
+      productName: item.name,
+      price: item.price,
+      image: item.image,
+      category: item.category,
+      email: "example@example.com",
+      status: "Pending",
+      date: new Date(),
+    };
+    fetch("http://loaclhost:5000/orders", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(orderData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertId) {
+          alert(isPet ? "Adoption request sent" : "Added to orders!");
+        }
+      });
+  };
+
+  if (!item) {
+    return <div className="text-center py-20">Loading...</div>;
+  }
+  const isPet = item.category === "Pet";
   return (
-    <div className="max-w-4xl mx-auto p-6 mt-10">
-      <div className="flex flex-col md:flex-row gap-10 bg-white shadow-xl rounded-3xl overflow-hidden">
-        <img
-          src={item.image}
-          alt={item.name}
-          className="md:w-1/2 h-96 object-cover"
-        />
-        <div className="p-8 space-y-3">
-          <h1 className="text-4xl font-black text-[#0a303a]">{item.name}</h1>
-          <p className="text-[#e83128] font-bold text-2xl">
-            {typeof item.price === "number" ? `$${item.price}` : item.price}
-          </p>
-          <p className="font-semibold">{item.location}</p>
-          <p className="text-black font-semibold">About {item.name}</p>
-          <p className="text-gray-600">{item.description}</p>
-          <button className="btn bg-[#e83128] text-white w-full border-none mt-2">
-            Adopt Now
-          </button>
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="max-w-4xl mx-auto p-6 mt-2"
+    >
+      <div className="max-w-4xl mx-auto p-6 mt-2">
+        <button
+          onClick={() => navigate(-1)}
+          className="flex items-center gap-2 text-gray-500 hover:text-[#e83128] font font-bold py-4 mb-6"
+        >
+          <IoArrowBack />
+          Go Back
+        </button>
+        <div className="flex flex-col md:flex-row gap-5 bg-white shadow-xl rounded-4xl overflow-hidden">
+          <img
+            src={item.image}
+            alt={item.name}
+            className="w-lg h-full object-cover hover:scale-105 transition-transform duration-700"
+          />
+          <div className="md:w-1/2 p-10 flex flex-col justify-center">
+            <div className="space-y-4">
+              <span className="bg-[#fceaea] text-[#e83128] px-4 py-1 rounded-full text-xs font-bold uppercase tracking-widest">
+                {item.category}
+              </span>
+              <h1 className="text-4xl font-black text-[#0a303a]">
+                {item.name}
+              </h1>
+              <div className="flex items-center text-gray-400 font-bold">
+                <IoLocationOutline className="text-[#e83128] mr-1 text-xl" />
+                {item.location || "Global Shipping"}
+              </div>
+              <p className="text-[#e83128] font-black text-3xl">
+                {typeof item.price === "number" ? `$${item.price}` : item.price}
+              </p>
+              <div className="py-3 border-t border-gray-100 mt-6">
+                <p className="text-[#0a303a] font-black uppercase text-sm tracking-widest">
+                  Description
+                </p>
+              </div>
+              <p className="text-gray-600 leading-relaxed italic mb-4">
+                {item.description}
+              </p>
+            </div>
+            <button
+              onClick={handleOrder}
+              className="bg-[#0a303a] hover:bg-[#e83128] text-white font-bold py-3 px-6 rounded-lg duration-300 text-sm"
+            >
+              {isPet ? "Adopt Now" : "Add To Cart"}
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- src/pages/PetSupply/PetSupply.jsx
+++ src/pages/PetSupply/PetSupply.jsx
@@ -5,6 +5,7 @@ const PetSupply = () => {
   const [products, setProducts] = useState([]);
   const [activeCat, setActiveCat] = useState("All");
   const [loading, setLoading] = useState(true);
+  const [searchQuery, setSearchQuery] = useState("");
 
   const categories = ["All", "Pet", "Accessory", "Pet Food", "Care Product"];
 
@@ -22,11 +23,22 @@ const PetSupply = () => {
         setLoading(false);
       });
   }, [activeCat]);
+  const filteredItems = products.filter((item) =>
+    item.name.toLowerCase().includes(searchQuery.toLowerCase()),
+  );
   return (
     <div className="max-w-7xl mx-auto px-4 py-10">
-      <h2 className="text-4xl font-black text-[#0a303a] mb-8 uppercase">
-        Pet <span className="text-[#e83128]">Supplies</span>
-      </h2>
+      <div className="flex flex-col md:flex-row justify-between items-center mb-8 gap-4">
+        <h2 className="text-4xl font-black text-[#0a303a] mb-8 uppercase">
+          Pet <span className="text-[#e83128]">Supplies</span>
+        </h2>
+        <input
+          type="text"
+          placeholder="search by name..."
+          className="px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#e83128] w-full md:w-64 text-gray-400"
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </div>
 
       <div className="flex flex-wrap gap-4 mb-12">
         {categories.map((cat) => (
@@ -39,31 +51,33 @@ const PetSupply = () => {
           </button>
         ))}
       </div>
+      {/* <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {filteredItems.map((item) => (
+          <ProductCard key={item._id} item={item}></ProductCard>
+        ))}
+      </div> */}
+
       {loading ? (
         <div className="text-center py-20">Loading Supplies...</div>
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          {products.map((products) => (
+          {products.map((item) => (
             <div
-              key={products.id}
+              key={item.id}
               className="border rounded-2xl p-4 hover:shadow-lg transition-shadow"
             >
               <img
-                src={products.image}
-                alt={products.name}
+                src={item.image}
+                alt={item.name}
                 className="h-48 w-full object-cover rounded-xl mb-4"
               />
               <span className="text-xs font-bold text-[#e83128] uppercase">
-                {products.category}
+                {item.category}
               </span>
-              <h3 className="font-bold text-lg text-[#0a303a]">
-                {products.name}
-              </h3>
-              <p className="text-[#0a303a] font-black mt-2">
-                ${products.price}
-              </p>
+              <h3 className="font-bold text-lg text-[#0a303a]">{item.name}</h3>
+              <p className="text-[#0a303a] font-black mt-2">${item.price}</p>
               <Link
-                to={`/details/${products._id}`}
+                to={`/details/${item._id}`}
                 className="block text-center mt-4 bg-[#0a303a] text-white py-2 rounded-lg text-sm hover:bg-[#e83128] transition-colors"
               >
                 View Product
